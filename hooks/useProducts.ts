171e74c0422a89953bb602958eb9881cbadb17f2
bautomass <33027482+bautomass@users.noreply.github.com>
2025-01-03 13:32:01 +0200

    modified:   hooks/useProducts.ts

--- hooks/useProducts.ts
+++ hooks/useProducts.ts
@@ -5,19 +5,19 @@ import { useCallback, useEffect, useState } from 'react';
 export function useProducts(initialProducts: Product[]) {
   const [products, setProducts] = useState<Product[]>(initialProducts || []);
 
+  // Add this effect to update products when initialProducts changes
   useEffect(() => {
     setProducts(initialProducts || []);
   }, [initialProducts]);
 
   const sortProducts = useCallback((direction: 'asc' | 'desc') => {
-    setProducts((prevProducts) => {
-      const sorted = [...prevProducts].sort((a, b) => {
+    setProducts(prevProducts => 
+      [...prevProducts].sort((a, b) => {
         const priceA = parseFloat(a.priceRange.minVariantPrice.amount);
         const priceB = parseFloat(b.priceRange.minVariantPrice.amount);
         return direction === 'asc' ? priceA - priceB : priceB - priceA;
-      });
-      return sorted;
-    });
+      })
+    );
   }, []);
 
   return {
