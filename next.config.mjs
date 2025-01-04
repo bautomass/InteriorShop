ca27b2b0c0638baec7752808273d06c81a2f8ab2
bautomass <33027482+bautomass@users.noreply.github.com>
2025-01-04 14:38:37 +0200

    modified:   next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
+    unoptimized: true,  // This will disable image optimization
     formats: ['image/avif', 'image/webp'],
     remotePatterns: [
       {
@@ -21,3 +22,37 @@ const nextConfig = {
 };
 
 export default nextConfig;
+
+
+
+
+
+
+
+
+
+
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   images: {
+//     formats: ['image/avif', 'image/webp'],
+//     remotePatterns: [
+//       {
+//         protocol: 'https',
+//         hostname: 'cdn.shopify.com',
+//         pathname: '/s/files/**'
+//       },
+//       {
+//         protocol: 'https',
+//         hostname: 'cdn.shopify.com',
+//         pathname: '/assets/**'
+//       }
+//     ]
+//   },
+//   transpilePackages: ['framer-motion'],
+//   poweredByHeader: false,
+//   reactStrictMode: true
+// };
+
+// export default nextConfig;
