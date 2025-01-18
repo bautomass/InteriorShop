b350a2e9712ae46dd7f9d1a0fb61677f41f8c932
bautomass <33027482+bautomass@users.noreply.github.com>
2025-01-18 19:32:44 +0200

    modified:   components/home/hero.tsx

--- components/home/hero.tsx
+++ components/home/hero.tsx
@@ -361,7 +361,7 @@ const HeroComponent = function Hero({}: HeroProps): JSX.Element {
 
                     {/* Lamp Image - Keep animation for visual interest */}
                     {index === 0 && slide.lampImage && (
-                      <motion.div
+                      <motion.div m
                         initial={{ opacity: 0, y: -50 }}
                         animate={{ 
                           opacity: currentSlide === index ? 1 : 0,
